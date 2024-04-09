5a50883011ef48ce378a2d73eea375fb9846ed3b
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2024-04-10 02:21:36 +0530

    Credits and View Cart Done

---
+++ easydine/components/ViewCart.jsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+export default function ViewCart() {
+
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+
+const styles = StyleSheet.create({
+
+})
